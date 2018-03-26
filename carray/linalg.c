57afb69fa138b561bc595edd8226fd6be4dc84e3
randptr <29264605+randptr@users.noreply.github.com>
2018-03-26 20:06:12 +0100

    Update linalg.c
    Return to avoid checking all the 'if' statements if unnecessary...

--- carray/linalg.c
+++ carray/linalg.c
@@ -36,7 +36,7 @@
  * @param n_b_cols  Number of cols in CArray B
  * @param b_ptr     CArray B MemoryPointer
  */
-void matmul(MemoryPointer * ptr, int n_a_rows, int n_a_cols, MemoryPointer * a_ptr, int n_b_cols, MemoryPointer *b_ptr) {
+void matmul(MemoryPointer * ptr, int n_a_rows, int n_a_cols, MemoryPointer * a_ptr, int n_b_cols, MemoryPointer * b_ptr) {
     int i, j, n_b_rows = n_a_cols;
     MemoryPointer bT_ptr;
     CArray a = ptr_to_carray(a_ptr);
@@ -51,6 +51,8 @@ void matmul(MemoryPointer * ptr, int n_a_rows, int n_a_cols, MemoryPointer * a_p
                 rtn.array2d[i][j] = cblas_sdot(n_a_cols, a.array2d[i], 1, bT.array2d[j], 1);
             }
         }
+      
+        return;
     }
     if(n_b_cols == 0 && n_a_cols > 0) {
         carray_init1d(n_a_rows, ptr);
@@ -58,6 +60,8 @@ void matmul(MemoryPointer * ptr, int n_a_rows, int n_a_cols, MemoryPointer * a_p
         for (i = 0; i < n_a_rows; ++i) {
             rtn.array1d[i] = cblas_sdot(n_a_cols, a.array2d[i], 1, b.array1d, 1);
         }
+      
+        return;
     }
     if(n_b_cols > 0 && n_a_cols == 0) {
         carray_init1d(n_a_rows, ptr);
@@ -67,10 +71,14 @@ void matmul(MemoryPointer * ptr, int n_a_rows, int n_a_cols, MemoryPointer * a_p
         for (i = 0; i < n_a_rows; ++i) {
             rtn.array1d[i] = cblas_sdot(n_a_rows, bT.array2d[i], 1,a.array1d, 1);
         }
+      
+        return;
     }
     if(n_b_cols == 0 && n_a_cols == 0) {
         carray_init0d(ptr);
         CArray rtn = ptr_to_carray(ptr);
         rtn.array0d[0] = cblas_sdot(n_a_rows, a.array1d, 1, b.array1d, 1);
+      
+        return;
     }
 }
