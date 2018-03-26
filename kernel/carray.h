3210d985ccae6794b247637cb4220b282abfcb40
Henrique Borba <henrisaviatto@gmail.com>
2018-03-25 22:34:11 -0300

    toArray and fromArray static methods now accept 1D arrays.

--- kernel/carray.h
+++ kernel/carray.h
@@ -38,9 +38,14 @@ typedef struct MemoryPointer {
     int uuid;
 } MemoryPointer;
 
-void destroy_carray(int uuid, int rows, int cols);
-void carray_to_array(CArray carray, zval * rtn_array, int m, int n);
+
 void carray_init(int rows, int cols, MemoryPointer * ptr);
+void carray_init1d(int width, MemoryPointer * ptr);
+void destroy_carray(int uuid, int rows, int cols);
+
 void array_to_carray_ptr(MemoryPointer * ptr, zval * inarray, int * rows, int * cols);
 CArray ptr_to_carray(MemoryPointer * ptr);
+
+void carray_to_array(CArray carray, zval * rtn_array, int m, int n);
+
 #endif //PHPSCI_EXT_CARRAY_H
