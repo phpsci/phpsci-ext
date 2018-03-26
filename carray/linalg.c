332b282a1e96e02b931fd6f5614c6abc0819ac71
Henrique Borba <henrisaviatto@gmail.com>
2018-03-26 01:34:22 -0300

    Implemented 1Dx2D matmul.

--- carray/linalg.c
+++ carray/linalg.c
@@ -39,15 +39,33 @@
 void matmul(MemoryPointer * ptr, int n_a_rows, int n_a_cols, MemoryPointer * a_ptr, int n_b_cols, MemoryPointer *b_ptr) {
     int i, j, n_b_rows = n_a_cols;
     MemoryPointer bT_ptr;
-    carray_init(n_a_rows, n_b_cols, ptr);
-    transpose(&bT_ptr, b_ptr, n_b_rows, n_b_cols);
-    CArray bT = ptr_to_carray(&bT_ptr);
     CArray a = ptr_to_carray(a_ptr);
     CArray b = ptr_to_carray(b_ptr);
-    CArray rtn = ptr_to_carray(ptr);
-    for (i = 0; i < n_a_rows; ++i) {
-        for (j = 0; j < n_b_cols; ++j) {
-            rtn.array2d[i][j] = cblas_sdot(n_a_cols, a.array2d[i], 1, bT.array2d[j], 1);
+    if(n_b_cols > 0 && n_a_cols > 0) {
+        carray_init(n_a_rows, n_b_cols, ptr);
+        transpose(&bT_ptr, b_ptr, n_b_rows, n_b_cols);
+        CArray bT = ptr_to_carray(&bT_ptr);
+        CArray rtn = ptr_to_carray(ptr);
+        for (i = 0; i < n_a_rows; ++i) {
+            for (j = 0; j < n_b_cols; ++j) {
+                rtn.array2d[i][j] = cblas_sdot(n_a_cols, a.array2d[i], 1, bT.array2d[j], 1);
+            }
+        }
+    }
+    if(n_b_cols == 0 && n_a_cols > 0) {
+        carray_init1d(n_a_rows, ptr);
+        CArray rtn = ptr_to_carray(ptr);
+        for (i = 0; i < n_a_rows; ++i) {
+            rtn.array1d[i] = cblas_sdot(n_a_cols, a.array2d[i], 1, b.array1d, 1);
+        }
+    }
+    if(n_b_cols > 0 && n_a_cols == 0) {
+        carray_init1d(n_a_rows, ptr);
+        CArray rtn = ptr_to_carray(ptr);
+        transpose(&bT_ptr, b_ptr, n_a_rows, n_b_cols);
+        CArray bT = ptr_to_carray(&bT_ptr);
+        for (i = 0; i < n_a_rows; ++i) {
+            rtn.array1d[i] = cblas_sdot(n_a_rows, bT.array2d[i], 1,a.array1d, 1);
         }
     }
 }
