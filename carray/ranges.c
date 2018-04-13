5afd06aa7a2e4548ff651903e08a40b06ad7bbf8
Henrique Borba <henrique.borba.dev@gmail.com>
2018-04-12 22:21:49 -0300

    Include Apache 2.0 license headers

--- carray/ranges.c
+++ carray/ranges.c
@@ -1,18 +1,23 @@
 /*
   +----------------------------------------------------------------------+
-  | PHP Version 7 | PHPSci                                               |
+  | PHPSci CArray                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 2018 Henrique Borba                                    |
+  | Copyright (c) 2018 PHPSci Team                                       |
   +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
+  | Licensed under the Apache License, Version 2.0 (the "License");      |
+  | you may not use this file except in compliance with the License.     |
+  | You may obtain a copy of the License at                              |
+  |                                                                      |
+  |     http://www.apache.org/licenses/LICENSE-2.0                       |
+  |                                                                      |
+  | Unless required by applicable law or agreed to in writing, software  |
+  | distributed under the License is distributed on an "AS IS" BASIS,    |
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      |
+  | implied.                                                             |
+  | See the License for the specific language governing permissions and  |
+  | limitations under the License.                                       |
   +----------------------------------------------------------------------+
-  | Author: Henrique Borba <henrique.borba.dev@gmail.com>                |
+  | Authors: Henrique Borba <henrique.borba.dev@gmail.com>               |
   +----------------------------------------------------------------------+
 */
 #include "ranges.h"
