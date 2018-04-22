a3fa718fb32ef6653a3cdcdbe86ece8ee442b6d5
Henrique Borba <henrique.borba.dev@gmail.com>
2018-04-22 13:20:03 +0000

    Standard Normal now only works with 1-D

--- operations/random.c
+++ operations/random.c
@@ -36,59 +36,11 @@
  */
 void standard_normal(MemoryPointer * ptr, int seed, int x, int y)
 {
-    int i, j;
-    srand(time(NULL)*seed);
+    int i, j, info;
     lapack_int seed_vector[4];
     if(x > 0 && y == 0) {
         carray_init1d(x, ptr);
         CArray new_array = ptr_to_carray(ptr);
-        LAPACKE_dlarnv(3, seed_vector, x, new_array.array1d);
+        info = LAPACKE_dlarnv(3, seed_vector, x, new_array.array1d);
     }
-    if(x > 0 && y > 0) {
-        carray_init(x, y, ptr);
-        CArray new_arr = ptr_to_carray(ptr);
-        for(i = 0; i < x; i++) {
-            for(j = 0; j < y; j++)
-                new_arr.array2d[(j * x) + i] = _randn(0.f, 1.0f);
-        }
-    }
-}
-
-
-/**
- * Return a sample CArray from the “standard normal” distribution.
- *
- * Based on: https://phoxis.org/2013/05/04/generating-random-numbers-from-normal-distribution-in-c/
- *
- * @author Henrique Borba
- * @param mu
- * @param sigma
- */
-double _randn (double mu, double sigma)
-{
-    double U1, U2, W, mult;
-    static double X1, X2;
-    static int call = 0;
-
-    if (call == 1)
-    {
-        call = !call;
-        return (double) (mu + sigma * (double) X2);
-    }
-
-    do
-    {
-        U1 = -1 + ((double) rand () / RAND_MAX) * 2;
-        U2 = -1 + ((double) rand () / RAND_MAX) * 2;
-        W = pow (U1, 2) + pow (U2, 2);
-    }
-    while (W >= 1 || W == 0);
-
-    mult = sqrt ((-2 * log (W)) / W);
-    X1 = U1 * mult;
-    X2 = U2 * mult;
-
-    call = !call;
-
-    return (mu + sigma * (double) X1);
 }
